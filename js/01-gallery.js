98e0a03f5a1f09452331c784143621182423dabf
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-23 16:42:49 +0300

    create markup render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,47 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+// gallery.addEventListener('click');
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryEl);
+
+// const gallery = document.querySelector('.gallery');
+// gallery.addEventListener('click', zoomImage);
+
+// const galleryEl = galleryItems
+//   .map(
+//     ({ description, original, preview }) =>
+//       `<li class = "gallery__item"><a class="gallery__link" href="${original}"><img class = "gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></li>`
+//   )
+//   .join('');
+
+// gallery.insertAdjacentHTML('beforeend', galleryEl);
+
+// function zoomImage(event) {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   console.log(event.target.dataset.source);
+
+//   const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}">`);
+//   instance.show();
+// }
