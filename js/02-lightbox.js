61f86aa6bfbfc06329cae220a81122e73bec8382
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-24 13:21:58 +0300

    hw complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryEl);
+
+let galleryBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
